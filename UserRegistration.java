ae7d684c3776596d782917584b3d2690f7aca826
Hrudaya Guttikonda <hrudaya_g@Hrudayas-MacBook-Air.local>
2025-12-19 15:06:14 +0530

    Added UC2 part for user reg

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,15 +3,29 @@ public class UserRegistration {
     public static boolean validateFirstName(String firstName) {
         return firstName.matches("[A-Z][a-zA-Z]{2,}");
     }
+
+    public static boolean validateLastName(String lastName) {
+        return lastName.matches("[A-Z][a-zA-Z]{2,}");
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter First Name: ");
         String firstName = sc.nextLine();
-        if (validateFirstName(firstName)) {
+
+        System.out.print("Enter Last Name: ");
+        String lastName = sc.nextLine();
+
+        if (validateFirstName(firstName))
             System.out.println("Valid First Name");
-        } else {
+        else
             System.out.println("Invalid First Name");
-        }
+
+        if (validateLastName(lastName))
+            System.out.println("Valid Last Name");
+        else
+            System.out.println("Invalid Last Name");
         sc.close();
     }
 }
