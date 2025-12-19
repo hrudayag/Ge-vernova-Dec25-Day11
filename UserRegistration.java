ee061abb94781c1e542feb8af92bbae0178b4283
Hrudaya Guttikonda <hrudaya_g@Hrudayas-MacBook-Air.local>
2025-12-19 15:09:11 +0530

    Added UC3 part for user reg

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,29 +3,24 @@ public class UserRegistration {
     public static boolean validateFirstName(String firstName) {
         return firstName.matches("[A-Z][a-zA-Z]{2,}");
     }
-
     public static boolean validateLastName(String lastName) {
         return lastName.matches("[A-Z][a-zA-Z]{2,}");
     }
-
+    public static boolean validateEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$";
+        return email.matches(emailRegex);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter First Name: ");
         String firstName = sc.nextLine();
-
         System.out.print("Enter Last Name: ");
         String lastName = sc.nextLine();
-
-        if (validateFirstName(firstName))
-            System.out.println("Valid First Name");
-        else
-            System.out.println("Invalid First Name");
-
-        if (validateLastName(lastName))
-            System.out.println("Valid Last Name");
-        else
-            System.out.println("Invalid Last Name");
+        System.out.print("Enter Email: ");
+        String email = sc.nextLine();
+        System.out.println(validateFirstName(firstName) ? "Valid First Name" : "Invalid First Name");
+        System.out.println(validateLastName(lastName) ? "Valid Last Name" : "Invalid Last Name");
+        System.out.println(validateEmail(email) ? "Valid Email" : "Invalid Email");
         sc.close();
     }
 }
